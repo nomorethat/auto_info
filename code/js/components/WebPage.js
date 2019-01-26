3facd9b98d61481ef8b4f6535bd0c841ec1efb3a
Дмитрий <dm_lis@mail.ru>
2019-01-26 13:22:10 +0300

    Создание основного компонента, создание провайдера и связь нашего основного компонета с хранилищем через провайдер

---
+++ code/js/components/WebPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WebPage = () => (
+    <div>
+        <h2>Cars: </h2>
+        <hr />
+        <h3>Details: </h3>
+    </div>
+);
+
+export default WebPage;
--- code/js/index.js
+++ code/js/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import {createStore} from "redux";
 import allReducers from './reducers';
+import {Provider} from 'react-redux';
+import WebPage from './components/WebPage';
 
 const store = createStore(allReducers);
 
 ReactDOM.render(
-  <h2>Здесь может быть любой текст</h2>, 
+  <Provider store={store}>
+      <WebPage />
+  </Provider>,
   document.getElementById('fieldToShow')
 );
