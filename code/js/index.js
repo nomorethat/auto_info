588a49e7e8ad7ebd24cbe1be5f6c7b5ab6605302
Дмитрий <dm_lis@mail.ru>
2019-01-26 13:12:53 +0300

    Создание store, массива данных, связанного с ним reducer'a

--- code/js/index.js
+++ code/js/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
   <h2>Здесь может быть любой текст</h2>, 
---
+++ code/js/reducers/car.js
@@ -0,0 +1,22 @@
+export default function () {
+    return[
+        {
+            id: 1,
+            name: "audi",
+            speed: 234,
+            weight: 1.8
+        }
+        {
+            id: 2,
+            name: "mersedes",
+            speed: 261,
+            weight: 1.6
+        }
+        {
+            id: 1,
+            name: "bmv",
+            speed: 254,
+            weight: 1.9
+        }
+    ]
+}
